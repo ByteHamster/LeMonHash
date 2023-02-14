de51875e7de204234e1206802242561f2eb37517
ByteHamster <info@bytehamster.com>
2023-02-14 18:18:54 +0100

    Add offset storage that explicitly stores positions of runs of duplicate values

--- include/support/PartitionedEliasFano.hpp
+++ include/support/PartitionedEliasFano.hpp
@@ -11,7 +11,7 @@
  * Relays actual storage of the integers to the child data structure.
  */
 template <typename ChildSequence>
-class DuplicateFilterList {
+class DuplicateFilterRank {
     private:
         static constexpr size_t DUPLICATE_FILTER_GRANULARITY = 3;
         size_t n = 0;
@@ -21,12 +21,12 @@ class DuplicateFilterList {
         pasta::FlatRankSelect<pasta::OptimizedFor::ONE_QUERIES> duplicateFilterRank;
         ChildSequence child;
     public:
-        DuplicateFilterList() {
+        DuplicateFilterRank() {
             duplicateFilterIsAreaStored.resize(100, false);
             duplicateFilterIsAreaStored[0] = true;
         }
 
-        DuplicateFilterList(DuplicateFilterList &&other) noexcept {
+        DuplicateFilterRank(DuplicateFilterRank &&other) noexcept {
             std::construct_at(&child, std::move(other.child));
             n = other.n;
             lastValueOfPreviousArea = other.lastValueOfPreviousArea;
@@ -106,6 +106,129 @@ class DuplicateFilterList {
                     + duplicateFilterIsAreaStored.size()
                     + 8 * duplicateFilterRank.space_usage()
                     + child.bit_size();
+        }
+};
+
+/**
+ * Stores the positions and offsets at which runs of duplicate values start.
+ * Runs need a size of at least MIN_DUPLICATES.
+ */
+template <typename ChildSequence>
+class DuplicateFilterRunlength {
+    private:
+        static constexpr size_t MIN_DUPLICATES = 8;
+        size_t n = 0;
+        size_t previousValue = -1;
+        size_t duplicatesOfPreviousValue = 0;
+        ChildSequence child;
+        std::vector<size_t> duplicateRunsStart;
+        util::EliasFanoM *duplicateRunsStartEf = nullptr;
+        std::vector<size_t> duplicateRunsLengthPrefix;
+        util::EliasFanoM *duplicateRunsLengthPrefixEf = nullptr;
+        size_t duplicateRunsLengthPrefixTmp = 0;
+    public:
+        DuplicateFilterRunlength() = default;
+
+        DuplicateFilterRunlength(DuplicateFilterRunlength &&other) noexcept {
+            std::construct_at(&child, std::move(other.child));
+            n = other.n;
+            previousValue = other.previousValue;
+            duplicatesOfPreviousValue = other.duplicatesOfPreviousValue;
+            duplicateRunsStart = std::move(other.duplicateRunsStart);
+            duplicateRunsStartEf = other.duplicateRunsStartEf;
+            other.duplicateRunsStartEf = nullptr;
+            duplicateRunsLengthPrefix = std::move(other.duplicateRunsLengthPrefix);
+            duplicateRunsLengthPrefixEf = other.duplicateRunsLengthPrefixEf;
+            other.duplicateRunsLengthPrefixEf = nullptr;
+            duplicateRunsLengthPrefixTmp = other.duplicateRunsLengthPrefixTmp;
+        }
+
+        void flushDuplicates() {
+            if (duplicatesOfPreviousValue < MIN_DUPLICATES) {
+                // Simply store them
+                for (size_t i = 0; i < duplicatesOfPreviousValue; i++) {
+                    child.append(previousValue);
+                }
+            } else {
+                duplicateRunsStart.push_back(n - duplicatesOfPreviousValue);
+                duplicateRunsLengthPrefix.push_back(duplicateRunsLengthPrefixTmp);
+                duplicateRunsLengthPrefixTmp += duplicatesOfPreviousValue;
+            }
+        }
+
+        void append(size_t x) {
+            if (x == previousValue) {
+                duplicatesOfPreviousValue++;
+            } else {
+                if (n != 0) {
+                    flushDuplicates();
+                }
+                child.append(x);
+                previousValue = x;
+                duplicatesOfPreviousValue = 0;
+            }
+            n++;
+        }
+
+        size_t size() const {
+            return n;
+        }
+
+        void complete() {
+            if (n == 0) {
+                return;
+            }
+            flushDuplicates();
+            duplicateRunsStart.push_back(n);
+            duplicateRunsLengthPrefix.push_back(duplicateRunsLengthPrefixTmp);
+
+            duplicateRunsStartEf = new util::EliasFanoM(duplicateRunsStart.size(),
+                               duplicateRunsStart.back() + 1);
+            for (size_t i = 0; i < duplicateRunsStart.size(); i++) {
+                duplicateRunsStartEf->push_back(duplicateRunsStart.at(i));
+            }
+            duplicateRunsStartEf->buildRankSelect();
+            duplicateRunsStart.resize(0);
+            duplicateRunsStart.shrink_to_fit();
+
+            duplicateRunsLengthPrefixEf = new util::EliasFanoM(duplicateRunsLengthPrefix.size(),
+                                duplicateRunsLengthPrefix.back() - (duplicateRunsLengthPrefix.size() - 1) * MIN_DUPLICATES + 1);
+            for (size_t i = 0; i < duplicateRunsLengthPrefix.size(); i++) {
+                duplicateRunsLengthPrefixEf->push_back(duplicateRunsLengthPrefix.at(i) - i * MIN_DUPLICATES);
+            }
+            duplicateRunsLengthPrefixEf->buildRankSelect();
+            duplicateRunsLengthPrefix.resize(0);
+            duplicateRunsLengthPrefix.shrink_to_fit();
+
+            child.complete();
+        }
+
+        size_t at(size_t i) {
+            if (duplicateRunsStartEf->size() == 0 || i < *duplicateRunsStartEf->at(0)) {
+                return child.at(i); // Before any run
+            }
+            auto predecessorPtr = duplicateRunsStartEf->predecessorPosition(i);
+            size_t predecessorIndex = predecessorPtr.index();
+            size_t runStart = *predecessorPtr;
+
+            auto numEmptyBeforePtr = duplicateRunsLengthPrefixEf->at(predecessorIndex);
+            size_t removedEmptyBefore = *numEmptyBeforePtr + predecessorIndex * MIN_DUPLICATES;
+            ++numEmptyBeforePtr;
+            size_t removedEmptyAfter = *numEmptyBeforePtr + (predecessorIndex + 1) * MIN_DUPLICATES;
+            size_t runStorageStart = runStart - removedEmptyBefore - 1;
+            size_t runSize = removedEmptyAfter - removedEmptyBefore;
+            size_t posInRun = i - runStart;
+            if (posInRun < runSize) {
+                return child.at(runStorageStart);
+            }
+            return child.at(runStorageStart + (posInRun - runSize) + 1);
+        }
+
+        size_t bit_size() {
+            return 8 * (sizeof(*this) - sizeof(ChildSequence))
+                   + 8 * duplicateRunsStartEf->space()
+                   + 8 * duplicateRunsLengthPrefixEf->space()
+                   + child.bit_size();
         }
 };
 
